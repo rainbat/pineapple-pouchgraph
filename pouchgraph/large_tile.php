adc577d58d9c4fb3a345dfdd92f24e23fe2db5d3
Dominique <info@rainbat.ch>
2015-08-02 21:16:39 +0200

    fixed for days smaller 10

--- pouchgraph/large_tile.php
+++ pouchgraph/large_tile.php
@@ -66,11 +66,12 @@ require($directory."includes/vars.php");
             var myVars=[];
             
             for(i=0;i<perLine.length-1;i++){
-                var line=perLine[i].split(" ");
+                var line=perLine[i].replace("   ","  ").split(" ");
                 ptime = line[3];
                 ptype = line[4];
                 pmac = line[7];
                 pssid = line[10];
+                console.log( "Log Zeile : " + i + " vom typ: " + ptype + " von MAC: " + pmac + " AN SSID: " + pssid); 
                 if(ptype == "Probe"){
                     if( pssid != null ){
                       pssid = pssid.replace("&#039;","");
